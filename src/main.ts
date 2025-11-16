0f424a6cc128cbad3bb460b515bae671e7311deb
Valery Semenenko <gearmobile@gmail.com>
2025-11-16 21:26:39 +0300

    chrore: add update support

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const host = '0.0.0.0';
-  // Получаем строковое значение PORT или '3000' по умолчанию
-  const portStr = process.env.PORT ?? '3000';
-  // Теперь parseInt получает строку
+  const host = '0.0.0.0'; // <-- для доступа извне контейнера
+  const portStr = process.env.PORT ?? '3000'; // <-- Безопасное получение строки
   const port = parseInt(portStr, 10);
 
-  // Опционально: проверить, является ли результат числом
   if (isNaN(port)) {
     console.error(`Invalid port value: ${portStr}`);
     process.exit(1);
