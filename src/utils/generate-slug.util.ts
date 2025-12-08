6a072bb56e73b54a1d0cb84e0c72554d29e13860
Valery Semenenko <gearmobile@gmail.com>
2025-12-08 21:34:34 +0300

    feat: config slug package

--- src/utils/generate-slug.util.ts
+++ src/utils/generate-slug.util.ts
@@ -2,7 +2,7 @@ import slug from 'slug';
 
 export function generateSlug(title: string): string {
   return (
-    slug(title, { lower: true }) +
+    slug(title, { lower: true, replacement: '-', trim: true }) +
     '-' +
     ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
   );
